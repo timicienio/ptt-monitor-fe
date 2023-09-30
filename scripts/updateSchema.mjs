2cc638c6cdd4f7ee72626fbb4c4a49fb3f98b223
timicienio <timicienio@25sprout.com>
2023-09-30 14:07:40 +0800

    chore: update schema

--- scripts/updateSchema.mjs
+++ scripts/updateSchema.mjs
@@ -1,7 +1,7 @@
 import "zx/globals";
 
-const apiSchemaEndpoint = "https://im-project.ddns.net/openapi.json";
+const apiSchemaEndpoint = "https://im-project.onrender.com/openapi.json";
 
-await $`wget  ${apiSchemaEndpoint} --directory-prefix=./temp`;
+await $`wget  ${apiSchemaEndpoint} -O ./temp/openapi.json`;
 await $`npx openapi-typescript ./temp/openapi.json -o ./types/schema.d.ts`;
 await $`rm -rf ./temp`;
